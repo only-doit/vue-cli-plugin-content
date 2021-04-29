f15b6f60d805161e348dc547f44658a005cc19ce
Antério Vieira <anteriovieira@gmail.com>
2021-04-29 18:25:57 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = (api, options) => {
+  api.chainWebpack(config => {
+    config.module.rules.delete("md");
+
+    const rule = config.module
+      .rule('md')
+      .test(/\.md$/)
+
+    rule
+      .use('vue-loader')
+        .loader('vue-loader')
+        .options({ /* ... */ })
+      
+    rule.use("markdown-loader")
+      .loader(require.resolve('@do-it/markdown-loader'))
+      .options({ sourceDir: './terms' })
+  });
+};
